d2205a127741c7c87b8a92cd7b0517a2edf67244
regno001 <rwt7454@gmail.com>
2025-05-29 01:23:56 +0530

    Add files via upload

---
+++ admin/menu.php
@@ -0,0 +1,41 @@
+<?php 
+include('../config/constants.php'); 
+include('login_check.php');     
+
+
+
+?>
+ 
+
+
+<html>
+
+<head>
+
+<title>Food Order Website Home Page</title>
+
+<link rel="stylesheet" href="../css/admin.css">
+
+</head>
+
+<div class="menu text-center">
+
+<div class="wrapper">
+
+<ul>
+
+<li><a href="index.php">Home</a></li>
+
+<li><a href="manage-admin.php">Admin</a></li>
+
+<li><a href="manage-category.php">Category</a></li>
+
+<li><a href="manage-food.php">Food</a></li>
+
+<li><a href="manage-order.php">Order</a></li>
+<li><a href="Logout.php">LOgout</a></li>
+</ul>
+
+</div>
+
+</div>
